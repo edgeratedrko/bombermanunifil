17afb368eca4ecdca42f027bdfffc8e46cd01769
victor ikeda <victor_ikeda_2008@hotmail.com>
2019-04-13 23:56:40 -0300

    apha 2

--- BomberMan.py
+++ BomberMan.py
@@ -1,24 +1,24 @@
 import pygame
 
-white=(255,255,255)
+white = (255,255,255)
 
 try:
 	pygame.init()
 except:
 	print("erro")	
-
-l=640
-a=480
+l = 800
+a = 600
 loop = True
 fundo = pygame.display.set_mode((l,a))
 pygame.display.set_caption("BomberMan")
+fundo_menu = pygame.image.load("bomberman.png")
 while loop:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			loop = False
-		else:
-			print(event)
-	fundo.fill(white)
-	pygame.display.update()
-
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        loop = False
+                else:
+                        print(event)
+        #fundo.fill(white)
+        fundo.blit(fundo_menu,[0,0])
+        pygame.display.update()
 pygame.quit()
